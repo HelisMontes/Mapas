bcb54b335c62922a42e315526b49b18665d5a4cd
Helis Montes <helismontes@gmail.com>
2021-05-19 17:23:11 -0500

    Mantener la referencia de latitud, longitud y zoom del mapa

--- app/src/pages/MapaPage.js
+++ app/src/pages/MapaPage.js
@@ -10,7 +10,8 @@ const puntoInicial = {
 
 export const MapaPage = () => {
   const mapDiv = useRef();
-  const [ , setMapa] = useState();
+  const [ mapa, setMapa] = useState();
+  const [coords, setCoords] = useState(puntoInicial);
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapDiv.current,
@@ -20,8 +21,22 @@ export const MapaPage = () => {
     });
     setMapa(map)
   }, [])
+  //Obtener las coordenadas
+  useEffect(() => {
+    mapa?.on('move', ()=>{
+      const {lng, lat} = mapa.getCenter();
+      setCoords({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: mapa.getZoom().toFixed(2)
+      })
+    });
+  }, [mapa])
   return (
     <>
+      <div className="info">
+        {`Lng: ${coords.lng} | Lat: ${coords.lat} | Zoom: ${coords.zoom}`}
+      </div>
       <div
         className="mapContainer"
         ref={mapDiv} 
